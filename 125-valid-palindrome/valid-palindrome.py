9aa76a75512819c068cec2b2e67fd72fc525a335
karunaav <167774140+karunaav@users.noreply.github.com>
2025-09-27 23:50:56 -0400

    Time: 163 ms (7.50%) | Memory: 13.1 MB (53.89%) - LeetSync

---
+++ 125-valid-palindrome/valid-palindrome.py
@@ -0,0 +1,9 @@
+class Solution(object):
+    def isPalindrome(self, s):
+       
+        newStr=''
+        for c in s:
+            if c.isalnum():
+                newStr+=c.lower()
+        return newStr==newStr[::-1]
+        
