dce40115746d9b5931c8ed3b9bb33ee144dae372
Karuna Venkatesh <167774140+karunaav@users.noreply.github.com>
2026-07-25 19:35:12 -0400

    Time: 2102 ms (37.66%) | Memory: 13.5 MB (16.41%) - LeetSync

--- 1-two-sum/two-sum.py
+++ 1-two-sum/two-sum.py
@@ -1,11 +1,8 @@
-class Solution:
+class Solution(object):
     def twoSum(self, nums, target):
-        
-        prevMap={}
-        for i,n in enumerate(nums):
-            diff=target-n
-            if diff in prevMap:
-                return [prevMap[diff],i]
-            prevMap[n]=i
-        return        
+        for i in range (len(nums)):
+            for j in range(i+1,len(nums)):
+                if nums[i]+nums[j]==target:
+                    return [i,j]
+        return []
         
